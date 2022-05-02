4de80442ee25779915e6db6847a02766a172dc60
ksxfexp <104796028+ksxfexp@users.noreply.github.com>
2022-05-02 23:53:09 +0300

    Create kefensploit.py
    Geliştirme aşamasında

---
+++ kefensploit.py
@@ -0,0 +1,45 @@
+import time
+import os
+import webbrowser
+from pyfiglet import Figlet
+from os import name
+import string
+from random import *
+import hashlib
+
+
+time.sleep(2)
+webbrowser.open('https://www.instagram.com/kemaaalwz/')
+
+time.sleep(2)
+
+f = Figlet(font='roman')
+print(f.renderText('kemalwz'))
+time.sleep(1)
+
+print("sınırsız yazı yazmak için 01 ")
+print("şifre oluşturmak için 02 ")
+print("md5 oluşturmak için 03")
+ 
+islm = input("işlem numarasını girin :")
+
+if islm == "01":
+    snrsz = input("neyi tekrarliyalım :")    
+    while True:
+        print(snrsz)
+ 
+if islm == "02":
+    print("şifreniz oluşturuyor % 50 ")     
+time.sleep(1)
+print("şifreniz oluşturuyor % 100 ")     
+characters = string.ascii_letters + string.punctuation + string.digits
+password = "".join(choice(characters) for x in range(randint(7, 14)))
+print(password)
+
+if islm == "03":
+    sifr = input("neyi hashliyelim :")
+time.sleep(1)
+sifrele=hashlib.md5()
+sifrele.update(sifr.encode("utf-8"))
+cikti=sifrele.hexdigest()
+print(cikti)
